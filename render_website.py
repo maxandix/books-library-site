cb822543877ba4ca2988175127fc90206e63a10c
Бирюков Максим <maxandix@gmail.com>
2020-05-19 00:28:52 +0300

    split site on pages

--- render_website.py
+++ render_website.py
@@ -2,6 +2,10 @@ from livereload import Server
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import json
 from more_itertools import chunked
+import os
+from os.path import join
+
+PAGES_DIR = 'pages'
 
 
 def on_reload():
@@ -14,12 +18,15 @@ def on_reload():
         books_info = json.loads(my_file.read())
 
     template = env.get_template('template.html')
-    rendered_page = template.render(chunks=chunked(books_info, 2))
-    with open('index.html', 'w', encoding="utf8") as file:
-        file.write(rendered_page)
+    for index, books_info_chunk in enumerate(chunked(books_info, 10)):
+        rendered_page = template.render(chunks=chunked(books_info_chunk, 2))
+        with open(join(PAGES_DIR, f'index{index}.html'), 'w', encoding="utf8") as file:
+            file.write(rendered_page)
 
 
 def main():
+    os.makedirs(PAGES_DIR, exist_ok=True)
+
     on_reload()
 
     server = Server()
