979f629f7390433abade33979e49af86d6066559
Бирюков Максим <maxandix@gmail.com>
2020-05-19 18:22:36 +0300

    add Pagination

--- render_website.py
+++ render_website.py
@@ -4,8 +4,10 @@ import json
 from more_itertools import chunked
 import os
 from os.path import join
+import math
 
 PAGES_DIR = 'pages'
+BOOKS_PER_PAGE = 10
 
 
 def on_reload():
@@ -17,9 +19,14 @@ def on_reload():
     with open("media/books_info.json", "r") as my_file:
         books_info = json.loads(my_file.read())
 
+    count_of_pages = math.ceil(len(books_info) / BOOKS_PER_PAGE)
     template = env.get_template('template.html')
-    for index, books_info_chunk in enumerate(chunked(books_info, 10)):
-        rendered_page = template.render(chunks=chunked(books_info_chunk, 2))
+    for index, books_info_chunk in enumerate(chunked(books_info, BOOKS_PER_PAGE), start=1):
+        rendered_page = template.render(
+            books=books_info_chunk,
+            count_of_pages=count_of_pages,
+            current_page=index,
+        )
         with open(join(PAGES_DIR, f'index{index}.html'), 'w', encoding="utf8") as file:
             file.write(rendered_page)
 
